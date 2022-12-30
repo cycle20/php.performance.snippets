3ef8106a887be8962901e1dbe394353abeec4d09
cycle20 <46718432+cycle20@users.noreply.github.com>
2022-12-30 15:30:48 +0100

    OpenCL init code: some info functions

--- gpu/opencl/main.c
+++ gpu/opencl/main.c
@@ -1,5 +1,178 @@
 #include <stdio.h>
 
+#define CL_TARGET_OPENCL_VERSION 120
+#include <CL/cl.h>
+
 int main(void) {
+    void getClInfo();
+
+    getClInfo();
+
     return 0;
 }
+
+void getClInfo() {
+    cl_platform_id * getClPlatforms(cl_uint *numberOfPlatforms);
+    char * getClPlatformParam(cl_platform_id platform, cl_platform_info paramName);
+
+    cl_platform_id* platformIds = NULL;
+    cl_uint numPlatforms = 0;
+
+    platformIds = getClPlatforms(&numPlatforms);
+    fprintf(stderr, "Number of OpenCL Platforms: %i\n\n", numPlatforms);
+
+    for (int platformIndex = 0; platformIndex < numPlatforms; platformIndex++) {
+
+        cl_platform_id platform = platformIds[platformIndex];
+        // printf("Platform ID: %d, ", *(int *)(void *)platform);
+        // printf("Platform ID: %d, ", (struct _cl_platform_id *)platform);
+        char *value = getClPlatformParam(platform, CL_PLATFORM_NAME);
+        fprintf(stderr, "Platform Name: %s\n", value);
+        free(value);
+
+        value = getClPlatformParam(platform, CL_PLATFORM_VENDOR);
+        fprintf(stderr, "    Vendor: %s\n", value);
+        free(value);
+
+        value = getClPlatformParam(platform, CL_PLATFORM_VERSION);
+        fprintf(stderr, "    Version: %s\n", value);
+        free(value);
+
+        value = getClPlatformParam(platform, CL_PLATFORM_PROFILE);
+        fprintf(stderr, "    Profile: %s\n", value);
+        free(value);
+
+        value = getClPlatformParam(platform, CL_PLATFORM_EXTENSIONS);
+        fprintf(stderr, "    Extensions: %s\n", value);
+        free(value);
+
+
+
+        cl_device_id * getClGPUDevices(cl_platform_id platform, cl_uint *numDevices);
+        cl_ulong getClDeviceParamULong(cl_device_id device, cl_device_info paramName);
+        cl_uint getClDeviceParamUInt(cl_device_id device, cl_device_info paramName);
+        size_t getClDeviceParamSizeT(cl_device_id device, cl_device_info paramName);
+        char * getClDeviceParamChar(cl_device_id device, cl_device_info paramName);
+
+
+        cl_uint numDevices = 0;
+        cl_device_id *devices = getClGPUDevices(platform, &numDevices);
+        if (devices == NULL) {
+            fprintf(stderr, "\n    GPU device not found\n");
+            continue;
+        }
+        fprintf(stderr, "\n    Number of GPU Devices: %i\n", numDevices);
+
+        for (int deviceIndex = 0; deviceIndex < numDevices; deviceIndex++) {
+            cl_device_id device = devices[deviceIndex];
+
+            char *charValue = getClDeviceParamChar(device, CL_DEVICE_NAME);
+            fprintf(stderr, "        Device Name: %s\n", charValue);
+            free(charValue);
+
+            cl_ulong longValue = getClDeviceParamULong(device, CL_DEVICE_MAX_MEM_ALLOC_SIZE);
+            fprintf(stderr, "        Max Mem Alloc Size: %ld Bytes\n", longValue);
+
+            longValue = getClDeviceParamULong(device, CL_DEVICE_LOCAL_MEM_SIZE);
+            fprintf(stderr, "        Local Mem Size: %ld Bytes\n", longValue);
+
+            cl_uint intValue = getClDeviceParamUInt(device, CL_DEVICE_MAX_COMPUTE_UNITS);
+            fprintf(stderr, "        Max Compute Units: %d\n", intValue);
+
+            size_t size = getClDeviceParamSizeT(device, CL_DEVICE_MAX_WORK_GROUP_SIZE);
+            fprintf(stderr, "        Max Work Group Size: %ld\n", size);
+
+            size = getClDeviceParamSizeT(device, CL_DEVICE_MAX_WORK_GROUP_SIZE);
+            fprintf(stderr, "        Max Work Group Size: %ld\n", size);
+        }
+
+        free(devices);
+    }
+
+}
+
+void clInit() {
+    // TODO
+}
+
+cl_platform_id * getClPlatforms(cl_uint *numPlatforms) {
+    cl_platform_id *platformIds = NULL;
+
+    clGetPlatformIDs(0, NULL, numPlatforms);
+    if (*numPlatforms == 0) {
+        fprintf(stderr, "No OpenCL platforms found.");
+
+        return NULL;
+    }
+
+    platformIds = malloc(sizeof(cl_platform_id) * *numPlatforms);
+    clGetPlatformIDs(*numPlatforms, platformIds, NULL);
+
+    return platformIds;
+}
+
+char * getClPlatformParam(cl_platform_id platform, cl_platform_info paramName) {
+    size_t valueSize = 0;
+    char *value = NULL;
+
+    clGetPlatformInfo(platform, paramName, 0, NULL, &valueSize);
+    value = calloc(valueSize + 1, sizeof(char));
+    clGetPlatformInfo(platform, paramName, valueSize, value, NULL);
+
+    return value;
+}
+
+cl_device_id * getClGPUDevices(cl_platform_id platform, cl_uint *numDevices) {
+    cl_int result;
+
+    result = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 0, NULL, numDevices);
+    if (result == CL_DEVICE_NOT_FOUND) {
+        return NULL;
+    }
+
+
+    cl_device_id *devices = malloc(sizeof(cl_device_id) * (*numDevices));
+    clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, *numDevices, devices, NULL);
+
+    return devices;
+}
+
+char * getClDeviceParamChar(cl_device_id device, cl_device_info paramName) {
+    size_t valueSize = 0;
+    char *value = NULL;
+
+    clGetDeviceInfo(device, paramName, 0, NULL, &valueSize);
+    value = calloc(valueSize + 1, sizeof(char));
+    clGetDeviceInfo(device, paramName, valueSize, value, NULL);
+
+    return value;
+}
+
+void getClDeviceParamNonTyped(cl_device_id device, cl_device_info paramName, void *result, size_t resultSize) {
+    clGetDeviceInfo(device, paramName, resultSize, result, NULL);
+}
+
+cl_ulong getClDeviceParamULong(cl_device_id device, cl_device_info paramName) {
+    cl_ulong result = 0;
+    getClDeviceParamNonTyped(device, paramName, &result, sizeof(result));
+
+    return result;
+}
+
+cl_uint getClDeviceParamUInt(cl_device_id device, cl_device_info paramName) {
+    cl_uint result = 0;
+    getClDeviceParamNonTyped(device, paramName, &result, sizeof(result));
+
+    return result;
+}
+
+size_t getClDeviceParamSizeT(cl_device_id device, cl_device_info paramName) {
+    size_t result = 0;
+    getClDeviceParamNonTyped(device, paramName, &result, sizeof(result));
+
+    return result;
+}
+
+
+// TODO: printPlatform
+// TODO: printDevice
