23b25ecf4203854db748cd2f417db3481b896d4f
cycle20 <46718432+cycle20@users.noreply.github.com>
2023-01-13 10:33:38 +0100

    Alignment: added print function

--- alignment/main.c
+++ alignment/main.c
@@ -1,20 +1,53 @@
 #include <stdio.h>
 
 #define ALIGNMENT8 8
+#define ALIGNMENT16 16
+#define YES "yes"
+#define NO "no"
 
-int main(void) {
-  unsigned int *i __attribute__((aligned (ALIGNMENT8)));
-  unsigned int j __attribute__((aligned (ALIGNMENT8))) = 117;
+int main(void)
+{
+  void printAddressInfo(void *ptr);
+
+  unsigned int *i __attribute__((aligned(ALIGNMENT8)));
+  unsigned int j __attribute__((aligned(ALIGNMENT8))) = 117;
 
   printf("int size: %ld; current alignment: %d, biggest alignment: %d\n",
-    sizeof(unsigned int),
-    ALIGNMENT8,
-    __BIGGEST_ALIGNMENT__);
+         sizeof(unsigned int),
+         ALIGNMENT8,
+         __BIGGEST_ALIGNMENT__);
 
   i = &j;
 
   printf("address of i: %p\naddress of j: %p\n", &i, i);
 
+  printAddressInfo(i);
 
   return 0;
+}
+
+void printAddressInfo(void *ptr)
+{
+  unsigned long address = (unsigned long)ptr;
+  unsigned int mask = 0;
+  char *answer;
+
+  printf("\n%p alignment details:\n", ptr);
+  printf("---------------------------------\n");
+
+  mask = 0x3;
+  answer = !(address & mask) ? YES : NO;
+  printf("%-20s %5s\n", "Is 4-byte aligned?", answer);
+
+  mask = 0x7;
+  answer = !(address & mask) ? YES : NO;
+  printf("%-20s %5s\n", "Is 8-byte aligned?", answer);
+
+  mask = 0xF;
+  answer = !(address & mask) ? YES : NO;
+  printf("%-20s %5s\n", "Is 16-byte aligned?", answer);
+
+  mask = 0x1F;
+  answer = !(address & mask) ? YES : NO;
+  printf("%-20s %5s\n", "Is 32-byte aligned?", answer);
 }
